9be77f29b1f93301075ddb192cd3ccce30bb025c
Mdarman899 <mdaminurislam517821@gmail.com>
2026-01-19 21:54:48 +0600

    best try for solve this problems

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,10 +1,10 @@
-import profile from '../../assets/images/profile.png'
+// import profile from '/projects/my-react-practise-project/public/images'
 
 const Header = () => {
     return (
         <div className='flex justify-between items-center mt-5 mx-1 border-b-2 bo '>
             <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
-            <img className='p-3' src={profile} alt="" />
+            <img className='p-3' src="/public/images/profile.png" alt="" />
         </div>
     );
 };
