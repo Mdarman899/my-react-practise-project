0a08beae57eacebf48774bffdd9bac2eed89f5bc
Mdarman899 <mdaminurislam517821@gmail.com>
2026-01-19 22:04:15 +0600

    Yes,almost done

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -4,7 +4,7 @@ const Header = () => {
     return (
         <div className='flex justify-between items-center mt-5 mx-1 border-b-2 bo '>
             <h1 className='text-4xl font-bold'>Knowledge Cafe</h1>
-            <img className='p-3' src="/public/images/profile.png" alt="" />
+            <img className='p-3' src="/images/profile.png" alt="" />
         </div>
     );
 };
